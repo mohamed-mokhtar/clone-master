79c8513176bdac03af2e99cd1b81cfb5c1f238c2
MOKHTAR <66872672+mohamed-mokhtar@users.noreply.github.com>
2025-12-24 15:29:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/clone-master/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
